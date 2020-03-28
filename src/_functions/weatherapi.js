d9a2856c6b79ac5bbb98a13757c9c9a85122f93a
seanneppl <sean.e.neppl@gmail.com>
2020-03-28 15:11:09 -0500

    another typo

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -5,11 +5,11 @@ exports.handler = async function (event, context, callback) {
    const { lat, lon } = event.queryStringParameters;
    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
 
-   const reponse = await axios.get(url);
+   const response = await axios.get(url);
 
    callback(null, {
       statusCode: 200,
-      body: JSON.stringify(reponse.data)
+      body: JSON.stringify(response.data)
    })
 
 }
