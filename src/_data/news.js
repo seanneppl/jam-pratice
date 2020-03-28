297e611c4dc4803db977eda15a3ac07352c4d2a1
seanneppl <sean.e.neppl@gmail.com>
2020-03-28 00:02:33 -0500

    localizations

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,23 @@
 const axios = require("axios");
 require("dotenv").config();
+const countries = require("./countries.json");
 
-module.exports = async function () {
+async function getNews(country) {
    try {
-      const response = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-28&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-      return response.data;
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+      return {
+         "country": country,
+         "articles": response.data.articles,
+      };
    } catch (error) {
       console.log(error);
    }
+};
+
+module.exports = async function () {
+   const newsPromises = countries.map(getNews);
+   return Promise.all(newsPromises).then(newsObjects => {
+      console.log("news", newsObjects);
+      return [].concat.apply([], newsObjects);
+   });
 };
